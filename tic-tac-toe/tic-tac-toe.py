bcb87556a63d455a44472e80f1889e913312a16b
Filip Gibarac <filip.gibarac@gmail.com>
2024-01-18 23:06:42 -0700

    update
    not sure if this is correct, but we're making slow progress? gotta figure out the disparity between tuples and matrices

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -13,6 +13,8 @@ class TicTacToe:
         self.moves = np.array([[' ', ' ', ' '],
                                [' ', ' ', ' '],
                                [' ', ' ', ' ']])
+        
+        self.history = dict.fromkeys(['Moves', 'Board'])
 
     def reset(self):
         # Reset the board to an initial state
@@ -24,18 +26,32 @@ class TicTacToe:
         self.moves = np.array([[' ', ' ', ' '],
                                [' ', ' ', ' '],
                                [' ', ' ', ' ']])
-
+                
+    def reset_history(self):
+        self.history = dict.fromkeys(['Moves', 'Board'])
+        
     def display(self):
         # Display the current state of the board
         print(self.board)
     
-    def get_possible_actions(self, state):
+    def get_possible_actions(self, state, moves, move):
         # Get available actions (empty cells) in the given state
         actions = []
         for i in range(3):
             for j in range(3):
                 if state[i][j] == ' ':
-                    actions.append((i, j))
+                    new_env_board = state.copy()
+                    new_env_moves = moves.copy()
+                    # i = j = 0
+                    new_env_moves[i][j] = move
+                    
+                    new_board_tuple = tuple(map(tuple, new_env_board))
+                    new_moves_tuple = tuple(map(tuple, new_env_moves))
+                    
+                    if (self.history['Board'] is None or new_board_tuple not in self.history['Board']) or \
+                    (self.history['Moves'] is None or new_moves_tuple not in self.history['Moves']):
+                        actions.append((i, j))
+
         return actions
     
     def is_empty(self, row, col):
@@ -90,21 +106,30 @@ class QLearningAgent:
                 self.q_table[(state, action)] = random.uniform(0, 1)  # Assign a random value between 0 and 1
 
     def get_q_value(self, state, action):
+        # Convert state and action to tuples
+        state_tuple = tuple(map(tuple, state))
+        action_tuple = tuple(action)
+        
         # Get the Q-value for a state-action pair from the Q-table
         # Initialize with a default value if not present
-        return self.q_table.get((state, action), 0.0)
+        print(agent.q_table.get((state_tuple, action_tuple), 0.0))
+        return self.q_table.get((state_tuple, action_tuple), 0.0)
 
     def choose_action(self, state, available_actions, moves):
         winning_moves = [action for action in available_actions if env.is_winner_move(action[0], action[1], state)]
         other_moves = [action for action in available_actions if action not in winning_moves]
 
-        if np.random.rand() < self.epsilon:
+        # if np.random.rand() < self.epsilon:
+        if np.random.rand() < agent.epsilon:
             if winning_moves:
                 action = random.choice(winning_moves)
             else:
                 action = random.choice(other_moves)
         else:
-            q_values = [self.get_q_value(state, action) for action in available_actions]
+            state_tuple = tuple(map(tuple, state))
+            #q_values = [self.get_q_value(state, action) for action in available_actions]
+            # q_values = [agent.get_q_value(state, action) for action in available_actions] #action = (0,0)
+            q_values = [agent.q_table.get((state_tuple, action), 0.0) for action in available_actions] #action = (0,0)
             action_index = np.argmax(q_values)
             action = available_actions[action_index]
 
@@ -116,12 +141,12 @@ class QLearningAgent:
 
         return action
 
-    def update_q_value(self, state, action, reward, next_state):
+    def update_q_value(self, state, action, reward, next_state, moves, move):
         # Update the Q-value using the Q-learning update rule
         current_q = self.get_q_value(state, action)
 
         # Check if there are possible actions in the next state
-        possible_actions_next = env.get_possible_actions(next_state)
+        possible_actions_next = env.get_possible_actions(next_state, moves, move)
 
         if possible_actions_next:
             max_future_q = max([self.get_q_value(next_state, next_action) for next_action in possible_actions_next])
@@ -141,7 +166,7 @@ def calculate_reward(env):
     elif env.is_winner('O'):
         return -1
     elif env.is_draw():
-        return 0
+        return -0.1
     else:
         return 0
 
@@ -153,13 +178,13 @@ def main():
 
     # Training loop
     global episodes
-    episodes = 362880  # Max potential actions allowed for  3x3 game, 362880
-    evaluation_interval = 50000  # Set your desired evaluation interval
+    episodes = 2  # Max potential actions allowed for  3x3 game, 362880
+    # evaluation_interval = 100000  # Set your desired evaluation interval
     
     # Initialize the Q-table with random values
-    all_possible_states = [tuple(map(tuple, np.zeros((3, 3))))]  # Add all possible states here
-    all_possible_actions = [(i, j) for i in range(3) for j in range(3)]  # Add all possible actions here
-    agent.random_initialize_q_table(all_possible_states, all_possible_actions)
+    # all_possible_states = [tuple(map(tuple, np.zeros((3, 3))))]  # Add all possible states here
+    # all_possible_actions = [(i, j) for i in range(3) for j in range(3)]  # Add all possible actions here
+    # agent.random_initialize_q_table(all_possible_states, all_possible_actions)
     print("Starting training")
     
     total_rewards = list()
@@ -167,60 +192,67 @@ def main():
     for episode in tqdm(range(episodes), desc="Training", unit=" episodes"):
         # print("Episode: " + str(episode + 1))
         env.reset()
-        if episode % evaluation_interval == 0 and episode != 0:
-            print(f"Evaluating model after {episode} episodes")
-            evaluate_agent(agent, env)
+        # if episode % evaluation_interval == 0 and episode != 0:
+        #     print(f"Evaluating model after {episode} episodes")
+        #     evaluate_agent(agent, env)
         
-        state = tuple(map(tuple, env.board))
+        # state = tuple(map(tuple, env.board))
+        state = env.board.copy()
         total_reward = 0
         symbol = 'X'
-
+        moves = np.zeros((3, 3))  # Initialize moves matrix for the current move
+        move = 1
+        
         while True:
-            move = 1
-            moves = np.zeros((3, 3))  # Initialize moves matrix for the current move
+
             reward = 0
-            # print("State:", state)
+            print("State:", state)
             
-            available_actions = env.get_possible_actions(state)
-            # print("Available actions:", available_actions)
+            available_actions = env.get_possible_actions(state, moves, move)
+            print("Available actions:", available_actions)
 
             if not available_actions:
                 break
 
             action = agent.choose_action(state, available_actions, moves)
-            # print("Chosen action:", action)
+            print("Chosen action:", action)
 
             row, col = action
             env.make_move(row, col, symbol, move)
+            # env.make_move(1, 1, 'X', 1)
 
-            next_state = tuple(map(tuple, env.board.copy()))
-            # print("Next state:", next_state)
+            # next_state = tuple(map(tuple, env.board.copy()))
+            next_state = env.board.copy()
+            print("Next state:", next_state)
 
             reward = calculate_reward(env)
-            # print("Reward:", reward)
+            print("Reward:", reward)
 
-            agent.update_q_value(state, action, reward, next_state)
+            agent.update_q_value(state, action, reward, next_state, moves, move)
 
             total_reward += reward
 
             if env.is_winner('X') or env.is_winner('O') or env.is_draw():
+                env.history['Moves'].append(tuple(map(tuple, moves)))
+                env.history['Board'].append(tuple(map(tuple, state)))
                 break
 
             symbol = 'O' if symbol == 'X' else 'X'
             state = next_state
             moves[row, col] = move  # Update moves matrix
             move += 1
+            print("Moves:", tuple(map(tuple, moves)))
 
         total_rewards.append(total_reward)
-
+        
         # Print progress occasionally
         # if episode % 10000 == 0:
         #    print(f"Episode: {episode}, Total Reward: {total_reward}")
-    # print("Total Reward:", total_rewards)
+    print("Total Reward:", total_rewards)
     print("Training complete.")
     
     # Print or plot the total rewards over episodes
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
     # Scale the total rewards by a factor of 10,000 for better readability
     # scaled_rewards = [reward / 10000 for reward in total_rewards]
@@ -241,21 +273,35 @@ def evaluate_agent(agent, env, player_symbol='X'):
     for _ in tqdm(range(episodes), desc="Evaluating", unit=" episodes"):
         env.reset()
         state = tuple(map(tuple, env.board))
+        print("State:", state)
+        move = 1
+        moves = np.zeros((3, 3))  # Initialize moves matrix for the current move    
         
         while True:
-            available_actions = env.get_possible_actions(state)
-            action = agent.choose_action(state, available_actions, np.zeros((3, 3)))  # Pass a dummy matrix for moves
+
+            available_actions = env.get_possible_actions(state, moves, move)
+            print("Available Actions:", available_actions)
+            
+            action = agent.choose_action(state, available_actions, moves)  # Pass a dummy matrix for moves
+            print("Action Chosen:", action)
             
             # Take the chosen action with the specified player symbol
-            env.make_move(action[0], action[1], player_symbol, 0)
+            row, col = action
+            env.make_move(row, col, player_symbol, move)
+        
             next_state = tuple(map(tuple, env.board.copy()))
+            print("Next state:", next_state)
 
             if env.is_winner('X') or env.is_winner('O') or env.is_draw():
                 break
-
+            
+            player_symbol = 'O' if player_symbol == 'X' else 'X'
             state = next_state
+            moves[row, col] = move  # Update moves matrix
+            move += 1
 
         total_rewards += calculate_reward(env)
+        print("Total Rewards:", total_rewards)
 
         if env.is_winner(player_symbol):
             total_wins += 1
@@ -278,4 +324,4 @@ if __name__ == "__main__":
     main()
 
     # Evaluation phase
-    evaluate_agent(agent, env)
+    # evaluate_agent(agent, env)
